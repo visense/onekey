e7920875320231466532a41dbb29e4adf17634f5
cocodex211 <visenselx@hotmail.com>
2023-12-20 23:22:00 +0800

    Update ccs.sh

--- ccs.sh
+++ ccs.sh
@@ -1,9 +1,18 @@
 #!/bin/bash
 
-# 创建 script.sh 并写入清理日志的命令
-cat <<EOF >/root/ccd.sh
+# 定义ccd.sh的完整路径
+CCD_SCRIPT_PATH="/root/ccd.sh"
+
+# 创建 ccd.sh 并写入清理日志的命令
+cat <<EOF >"$CCD_SCRIPT_PATH"
 #!/bin/bash
 # 删除超过30天的 syslog 和 daemon.log 文件，包括它们的旧版本
 find /var/log -type f \( -name "syslog*" -o -name "daemon.log*" \) -mtime +30 -exec rm -f {} \;
 EOF
+
+# 使 ccd.sh 可执行
+chmod +x "$CCD_SCRIPT_PATH"
+
+echo "$CCD_SCRIPT_PATH 已创建并设置为可执行。"
+
 

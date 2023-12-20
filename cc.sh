69284cb13084d6f7614af46bd28a9acf1d43e430
cocodex211 <visenselx@hotmail.com>
2023-12-20 22:50:26 +0800

    Update cc.sh

--- cc.sh
+++ cc.sh
@@ -3,7 +3,8 @@
 # 创建 script.sh 并写入清理日志的命令
 cat <<EOF >/root/script.sh
 #!/bin/bash
-find /var/log -name "*.log" -type f -mtime +30 -exec rm -f {} \;
+# 删除超过30天的 syslog 和 daemon.log 文件，包括它们的旧版本
+find /var/log -type f \( -name "syslog*" -o -name "daemon.log*" \) -mtime +30 -exec rm -f {} \;
 EOF
 
 # 使 script.sh 可执行

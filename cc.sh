e4e7dc1c2c2dd43ca78ed9557a148bc6bf95455d
cocodex211 <visenselx@hotmail.com>
2023-12-20 23:22:13 +0800

    Update cc.sh

--- cc.sh
+++ cc.sh
@@ -1,17 +1,20 @@
 #!/bin/bash
 
-# 检查 /root 下是否存在 cc.sh，如果存在则删除
-if [ -f "/root/ccs.sh" ]; then
-    rm -f /root/ccs.sh
+# 定义ccs.sh的完整路径
+CCS_SCRIPT_PATH="/root/ccs.sh"
+
+# 检查 ccs.sh 是否存在，如果存在则删除
+if [ -f "$CCS_SCRIPT_PATH" ]; then
+    rm -f "$CCS_SCRIPT_PATH"
 fi
 
-# 从GitHub下载最新的 cc.sh 到 /root 目录
-wget -O /root/ccs.sh https://raw.githubusercontent.com/visense/onekey/main/ccs.sh
+# 从GitHub下载最新的 ccs.sh 到 /root 目录
+wget -O "$CCS_SCRIPT_PATH" https://raw.githubusercontent.com/visense/onekey/main/ccs.sh
 
-# 使 /root/cc.sh 可执行
-chmod +x /root/ccs.sh
+# 使 ccs.sh 可执行
+chmod +x "$CCS_SCRIPT_PATH"
 
-# 添加 cron 任务，每天凌晨0:30分运行 /root/cc.sh
-(crontab -l 2>/dev/null; echo "30 0 * * * /root/ccs.sh") | crontab -
+# 添加 cron 任务，每天凌晨0:30分运行 ccs.sh
+(crontab -l 2>/dev/null; echo "30 0 * * * $CCS_SCRIPT_PATH") | crontab -
 
-echo "/root/ccs.sh 已更新并设置了每天凌晨0:30分的 cron 任务。"
+echo "$CCS_SCRIPT_PATH 已更新并设置了每天凌晨0:30分的 cron 任务。"
